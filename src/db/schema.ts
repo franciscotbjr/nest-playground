cd4fad92852b9dd390419e74492df0174c62df5e
Francisco Tarcizo Bomfim Júnior <francisco.franciscotbjr@gmail.com>
2026-05-25 20:23:37 -0300

    Added MCP weather module

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,21 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { integer, pgTable, varchar, boolean, uuid } from 'drizzle-orm/pg-core';
 import {} from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    id: uuid().primaryKey().defaultRandom(),
     name: varchar({length: 255}).notNull(),
     age: integer().notNull(),
     email: varchar({length: 255}).notNull().unique(),
+    emailUnverified: boolean().default(false),
+});
+
+export const postTable = pgTable("post", {
+    id: uuid().primaryKey().defaultRandom(),
+    createdBy: uuid().references(() => usersTable.id),
+});
+
+export const postReadTable = pgTable("postRead", {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid().references(() => usersTable.id),
+    postId: uuid().references(() => postTable.id),
 });
